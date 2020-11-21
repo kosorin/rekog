41325914e1d1dc2e6e50a00c1922b87529d0ebbc
David Kosorin <david@kosorin.net>
2020-11-21 17:56:15 +0100

    Automatically find all commands and add them to the root command

--- src/Rekog/Program.cs
+++ src/Rekog/Program.cs
@@ -1,28 +1,44 @@
-﻿using Rekog.Commands.Corpus;
+﻿using Rekog.Commands;
+using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Rekog
 {
     internal static class Program
     {
-        private static async Task<int> Main(string[] args)
+        private static Task<int> Main(string[] args)
         {
-            return await BuildCommandLine()
+            return BuildCommandLine()
                 .UseDefaults()
                 .Build()
-                .InvokeAsync(@"corpus D:\");
+                .InvokeAsync(args);
         }
 
         private static CommandLineBuilder BuildCommandLine()
         {
-            var root = new RootCommand
+            var root = new RootCommand();
+
+            foreach (var command in GetCommands())
             {
-                new CorpusCommand(),
-            };
+                root.AddCommand(command);
+            }
+
             return new CommandLineBuilder(root);
+        }
+
+        private static IEnumerable<Command> GetCommands()
+        {
+            var baseType = typeof(ControllerCommand<,>);
+            return baseType.Assembly.GetTypes()
+                .Where(x => x.BaseType != null
+                         && x.BaseType.IsGenericType
+                         && x.BaseType.GetGenericTypeDefinition() == baseType)
+                .Select(x => Activator.CreateInstance(x) is Command command ? command : throw new NullReferenceException());
         }
     }
 }
