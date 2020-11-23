ca40983e9a4da663814f667ed86f754a80e55cfa
David Kosorin <david@kosorin.net>
2020-11-23 17:38:22 +0100

    Add PathHelper unit tests

--- tests/Rekog.UnitTests/IO/PathHelperTests.cs
+++ tests/Rekog.UnitTests/IO/PathHelperTests.cs
@@ -69,6 +69,23 @@ namespace Rekog.UnitTests.IO
         }
 
         [Fact]
+        public void GetPaths_Directory_DefaultSearchPattern()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                [@"T:\ROOT.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\A.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\B.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\C.gif"] = new MockFileData(string.Empty),
+                [@"T:\Test\Sub1\D.txt"] = new MockFileData(string.Empty),
+            });
+
+            var paths = PathHelper.GetPaths(fileSystem, @"T:\Test", searchPattern: PathHelper.DefaultSearchPattern);
+
+            paths.ShouldBe(new[] { @"T:\Test\A.txt", @"T:\Test\B.txt", @"T:\Test\C.gif" }, ignoreOrder: true);
+        }
+
+        [Fact]
         public void GetPaths_Directory_TxtSearchPattern()
         {
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
@@ -104,6 +121,27 @@ namespace Rekog.UnitTests.IO
             var paths = PathHelper.GetPaths(fileSystem, @"T:\Test", recurseSubdirectories: true);
 
             paths.ShouldBe(new[] { @"T:\Test\A.txt", @"T:\Test\B.txt", @"T:\Test\C.gif", @"T:\Test\Sub1\D.txt", @"T:\Test\Sub1\E.txt", @"T:\Test\Sub3\F.gif", @"T:\Test\Sub3\Sub1\G.gif" }, ignoreOrder: true);
+        }
+
+        [Fact]
+        public void GetPaths_Directory_GifSearchPattern_RecurseSubdirectories()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                [@"T:\ROOT.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\A.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\B.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\C.gif"] = new MockFileData(string.Empty),
+                [@"T:\Test\Sub1\D.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\Sub1\E.txt"] = new MockFileData(string.Empty),
+                [@"T:\Test\Sub2\"] = new MockDirectoryData(),
+                [@"T:\Test\Sub3\F.gif"] = new MockFileData(string.Empty),
+                [@"T:\Test\Sub3\Sub1\G.gif"] = new MockFileData(string.Empty),
+            });
+
+            var paths = PathHelper.GetPaths(fileSystem, @"T:\Test", searchPattern: "*.gif", recurseSubdirectories: true);
+
+            paths.ShouldBe(new[] { @"T:\Test\C.gif", @"T:\Test\Sub3\F.gif", @"T:\Test\Sub3\Sub1\G.gif" }, ignoreOrder: true);
         }
     }
 }
