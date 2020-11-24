cd7f09f900042bdf4b101ab19365ab02d07ca09e
David Kosorin <david@kosorin.net>
2020-11-24 22:53:36 +0100

    Add encoding to PathConfig

--- src/Rekog/Input/Configurations/PathConfig.cs
+++ src/Rekog/Input/Configurations/PathConfig.cs
@@ -11,12 +11,15 @@ namespace Rekog.Input.Configurations
         public string SearchPattern { get; set; }
 
         public bool RecurseSubdirectories { get; set; }
+
+        public string Encoding { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         protected override void FixSelf()
         {
             Path ??= string.Empty;
             SearchPattern ??= PathHelper.DefaultSearchPattern;
+            Encoding ??= System.Text.Encoding.UTF8.WebName;
         }
 
         protected override IEnumerable<Input> CollectChildren()
