2f8b0e516b5c6b70e071388337ebf7fc3e024104
David Kosorin <david@kosorin.net>
2021-02-11 17:03:42 +0100

    Add extension and converter for enlarging geometry

---
+++ src/Rekog.App/Converters/EnlargedGeometryConverter.cs
@@ -0,0 +1,29 @@
+﻿using Rekog.App.Extensions;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Rekog.App.Converters
+{
+    public class EnlargedGeometryConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not Geometry geometry)
+            {
+                return null;
+            }
+            if (parameter is not double size)
+            {
+                return geometry;
+            }
+            return geometry.GetEnlargedPathGeometry(size);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Rekog.App/Extensions/GeometryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media;
+
+namespace Rekog.App.Extensions
+{
+    public static class GeometryExtensions
+    {
+        public static PathGeometry GetEnlargedPathGeometry(this Geometry geometry, double size)
+        {
+            return GetEnlargedPathGeometry(geometry, size, false);
+        }
+
+        public static PathGeometry GetEnlargedPathGeometry(this Geometry geometry, double size, bool round)
+        {
+            var widened = geometry.GetWidenedPathGeometry(new Pen(Brushes.Black, size * 2) { LineJoin = round ? PenLineJoin.Round : PenLineJoin.Miter }).GetOutlinedPathGeometry();
+            if (widened.Figures.Count == 2)
+            {
+                widened.Figures.RemoveAt(size > 0 ? 0 : 1);
+            }
+
+            return widened.GetFlattenedPathGeometry();
+        }
+    }
+}
