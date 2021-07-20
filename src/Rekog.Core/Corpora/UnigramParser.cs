67ca4879d2c9bc4189e9906f5eced2ab00f4687f
David Kosorin <david@kosorin.net>
2021-07-20 07:09:59 +0200

    Optimize UnigramParser
    Add ngram value cache to reduce new string allocation.

--- src/Rekog.Core/Corpora/UnigramParser.cs
+++ src/Rekog.Core/Corpora/UnigramParser.cs
@@ -1,9 +1,13 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Rekog.Core.Corpora
 {
     public class UnigramParser : INgramParser
     {
+        private static readonly ConcurrentDictionary<char, string> NgramValueCache = new ConcurrentDictionary<char, string>();
+
         public void Skip()
         {
             // Nothing to do
@@ -11,8 +15,23 @@ namespace Rekog.Core.Corpora
 
         public bool Next(char character, [MaybeNullWhen(false)] out string ngramValue)
         {
-            ngramValue = character.ToString();
+            ngramValue = GetCachedNgramValue(character);
+
+            // Always return true
             return true;
+        }
+
+        private static string GetCachedNgramValue(char character)
+        {
+            if (NgramValueCache.TryGetValue(character, out var ngramValue))
+            {
+                return ngramValue;
+            }
+
+            ngramValue = character.ToString();
+            NgramValueCache.TryAdd(character, ngramValue);
+
+            return ngramValue;
         }
     }
 }
