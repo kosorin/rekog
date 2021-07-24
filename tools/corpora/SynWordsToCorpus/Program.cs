689c9e8baa3db2814b5a127fa25e393618f12c29
David Kosorin <david@kosorin.net>
2021-07-25 01:57:43 +0200

    Add ČNK SYN2020 corpus tool

---
+++ tools/corpora/SynWordsToCorpus/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: .exe <input-path> <output-path>");
+}
+
+var input = args[0];
+var output = args[1];
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Delimiter = ";",
+    HasHeaderRecord = false,
+};
+
+using var writer = new StreamWriter(output);
+using var reader = new StreamReader(input);
+using var csv = new CsvReader(reader, config);
+
+Console.WriteLine("start");
+var line = 0;
+while (csv.Read())
+{
+    line++;
+    if (line % 100_000 == 0)
+    {
+        Console.WriteLine(line);
+    }
+
+    var word = csv.GetField<string>(1);
+    var count = csv.GetField<int>(2);
+
+    for (var i = 0; i < count; i++)
+    {
+        writer.WriteLine(word);
+    }
+}
+Console.WriteLine("end");
