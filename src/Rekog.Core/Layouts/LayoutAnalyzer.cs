a8c3f49507d77fb50cbee3cfaba4e97b1c25a6a5
David Kosorin <david@kosorin.net>
2021-07-24 18:09:45 +0200

    Remove useless analyzer

--- src/Rekog.Core/Layouts/LayoutAnalyzer.cs
+++ src/Rekog.Core/Layouts/LayoutAnalyzer.cs
@@ -25,7 +25,6 @@ namespace Rekog.Core.Layouts
 
                 // Row
                 new RowFrequencyAnalyzer(),
-                new SameHandJumpAnalyzer(),
 
                 // Hand
                 new HandAlternationAnalyzer(),
