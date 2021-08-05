b3c88bd6739cf93fbd98de858ab6ce2968a4754b
David Kosorin <david@kosorin.net>
2021-08-05 03:07:28 +0200

    Refactor App class

--- src/Rekog.App/App.xaml.cs
+++ src/Rekog.App/App.xaml.cs
@@ -1,7 +1,20 @@
-﻿namespace Rekog.App
+﻿using Rekog.App.View;
+using System.Windows;
+using Rekog.App.ViewModel;
+
+namespace Rekog.App
 {
     public partial class App
     {
         public static double UnitSize => (double)Current.Resources["UnitSize"];
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.DataContext = new MainViewModel();
+            MainWindow.Show();
+        }
     }
 }
