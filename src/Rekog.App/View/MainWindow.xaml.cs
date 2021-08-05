a179b99240f053bbc66f233d1049caf5d7e3778b
David Kosorin <david@kosorin.net>
2021-08-05 22:28:51 +0200

    Update design

--- src/Rekog.App/View/MainWindow.xaml.cs
+++ src/Rekog.App/View/MainWindow.xaml.cs
@@ -1,68 +1,10 @@
-﻿using System.Windows.Controls;
-
-namespace Rekog.App.View
+﻿namespace Rekog.App.View
 {
     public partial class MainWindow
     {
-        private bool _changingKleInput;
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void KleInput_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (KleRawDataList == null)
-            {
-                return;
-            }
-
-            if (_changingKleInput)
-            {
-                return;
-            }
-
-            try
-            {
-                _changingKleInput = true;
-
-                KleRawDataList.SelectedIndex = 0;
-            }
-            finally
-            {
-                _changingKleInput = false;
-            }
-        }
-
-        private void KleRawDataList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (KleInput == null)
-            {
-                return;
-            }
-
-            if (_changingKleInput)
-            {
-                return;
-            }
-
-            try
-            {
-                _changingKleInput = true;
-
-                KleInput.Text = KleRawDataList.SelectedItem is ComboBoxItem { Tag: string text, }
-                    ? text
-                    : null;
-                if (KleParseRawDataButton.Command.CanExecute(KleParseRawDataButton.CommandParameter))
-                {
-                    KleParseRawDataButton.Command.Execute(KleParseRawDataButton.CommandParameter);
-                }
-            }
-            finally
-            {
-                _changingKleInput = false;
-            }
         }
     }
 }
---
+++ src/Rekog.App/View/Panes/BoardPane.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Rekog.App.View.Panes
+{
+    public partial class BoardPane
+    {
+        public BoardPane()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem { Tag: string text, })
+            {
+                if (KleParseRawDataButton.Command.CanExecute(text))
+                {
+                    KleParseRawDataButton.Command.Execute(text);
+                }
+            }
+        }
+    }
+}
