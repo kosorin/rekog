dd2af5dc714eba6cd342450c9e2e71aa78b5aadd
David Kosorin <david@kosorin.net>
2021-08-11 01:03:02 +0200

    Add "layers" property to "board" model

--- src/Rekog.App/Model/BoardModel.cs
+++ src/Rekog.App/Model/BoardModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
 using Rekog.App.Model.Kle;
 using Rekog.App.ObjectModel;
 
@@ -10,6 +11,7 @@ namespace Rekog.App.Model
         private string _author = string.Empty;
         private string _notes = string.Empty;
         private string _background = "#808080";
+        private ObservableCollection<string> _layers = new ObservableCollection<string>();
         private ObservableObjectCollection<KeyModel> _keys = new ObservableObjectCollection<KeyModel>();
 
         public string Name
@@ -36,6 +38,12 @@ namespace Rekog.App.Model
             set => Set(ref _background, value);
         }
 
+        public ObservableCollection<string> Layers
+        {
+            get => _layers;
+            set => Set(ref _layers, value);
+        }
+
         public ObservableObjectCollection<KeyModel> Keys
         {
             get => _keys;
@@ -50,6 +58,21 @@ namespace Rekog.App.Model
                 Author = kleBoard.Author,
                 Notes = kleBoard.Notes,
                 Background = kleBoard.Background,
+                Layers = new ObservableCollection<string>(new[]
+                {
+                    "Top Left",
+                    "Top Center",
+                    "Top Right",
+                    "Middle Left",
+                    "Middle Center",
+                    "Middle Right",
+                    "Bottom Left",
+                    "Bottom Center",
+                    "Bottom Right",
+                    "Front Left",
+                    "Front Center",
+                    "Front Right",
+                }),
                 Keys = new ObservableObjectCollection<KeyModel>(kleBoard.Keys.Select(KeyModel.FromKle)),
             };
         }
