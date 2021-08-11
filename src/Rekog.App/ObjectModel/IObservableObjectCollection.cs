9e056427903b588482bab7373d48357ae8333890
David Kosorin <david@kosorin.net>
2021-08-11 03:09:52 +0200

    Add missing inherited interfaces to IObservableObjectCollection

--- src/Rekog.App/ObjectModel/IObservableObjectCollection.cs
+++ src/Rekog.App/ObjectModel/IObservableObjectCollection.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace Rekog.App.ObjectModel
 {
-    public interface IObservableObjectCollection : ICollection
+    public interface IObservableObjectCollection : ICollection, INotifyCollectionChanged, INotifyPropertyChanged
     {
         event CollectionItemChangedEventHandler? CollectionItemChanged;
 
