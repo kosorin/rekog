a9ae11bc7dcd774e1674efa2e1e9d26f5e3ddf92
David Kosorin <david@kosorin.net>
2021-08-14 23:52:41 +0200

    Add ObjectToList converter

---
+++ src/Rekog.App/Converters/ObjectToListConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Rekog.App.Converters
+{
+    [ValueConversion(typeof(object), typeof(IList))]
+    public class ObjectToListConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new[] { value, };
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IList { Count: >0, } list)
+            {
+                return list[0];
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
